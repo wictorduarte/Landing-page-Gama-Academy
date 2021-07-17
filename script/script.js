faf1f71e05271bf86c150334f153764d4d238ee2
Wictor Duarte <wictorduarte@outlook.com>
2021-07-17 11:01:30 -0300

    modificação na function submit e na responsividade

--- script/script.js
+++ script/script.js
@@ -1,4 +1,10 @@
 
+//Limpar o localStorage sempre que a página for fechada ou atualizada
+
+/*window.onbeforeunload = function(){
+    localStorage.clear();
+}
+*/
 function btnSubmit(){
 
     const getEmail = document.getElementById('email').value;
@@ -10,7 +16,8 @@ function btnSubmit(){
     else{
         alert('Para receber as ofertas, informe seu email!');
     }
-    document.getElementById('email').value = '';
+    //altera o valor do email para vazio após atualização ou fechamento da página
+    //document.getElementById('email').value = '';
 }
 
     
